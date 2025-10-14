bfcc2f442fbab41ec234fb8b3a4623186b92edbc
thomasm0 <mills.thoma@gmail.com>
2025-10-14 13:47:13 +0000

    Deploying to gh-pages from @ Amsterdam/ee-ads-rhf@4f60828b176dddd4bb950d8d4f7db96206024cd3 🚀

---
+++ demo-npm-updates/assets/SelectControl.stories-DO06UzhW.js
@@ -0,0 +1,146 @@
+import{j as o}from"./iframe-OO432EJY.js";import{u as c,F as m,S as g}from"./index.esm-DQSCKQ9i.js";import"./preload-helper-PPVm8Dsz.js";const i=({label:s="Type",name:r="example",options:t=[],description:d,registerOptions:u,wrapperProps:v,...b})=>{const p=c({defaultValues:{[r]:""}});return o.jsx(m,{...p,children:o.jsx(g,{label:s,name:r,options:t,description:d,registerOptions:u,wrapperProps:v,...b})})};i.__docgenInfo={description:"",methods:[],displayName:"SelectControl",props:{label:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"'Type'",computed:!1}},name:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"'example'",computed:!1}},options:{defaultValue:{value:"[]",computed:!1},required:!1}}};const y={title:"Components/SelectControl",component:i,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{label:{control:"text",description:"The checkbox label"},description:{control:"text",description:"Optional description text"},disabled:{control:"boolean"}}},e={args:{label:"Province",name:"province",options:[{label:"Groningen",value:"gr"},{label:"Fryslân (Friesland)",value:"fr"},{label:"Drenthe",value:"dr"},{label:"Overijssel",value:"ov"},{label:"Flevoland",value:"fl"},{label:"Gelderland",value:"ge"},{label:"Utrecht",value:"ut"},{label:"Noord-Holland",value:"nh"},{label:"Zuid-Holland",value:"zh"},{label:"Zeeland",value:"ze"},{label:"Noord-Brabant",value:"nb"},{label:"Limburg",value:"li"}],registerOptions:{required:"A province is required"},wrapperProps:{}}},l={args:{label:"Province",name:"province1",options:["Groningen","Fryslân (Friesland)","Drenthe","Overijssel","Flevoland","Gelderland","Utrecht","Noord-Holland","Zuid-Holland","Zeeland","Noord-Brabant","Limburg"],registerOptions:{required:"A province is required"},wrapperProps:{}}},n={args:{label:"Province",name:"province2",options:[{label:"Groningen",value:"gr"},{label:"Fryslân (Friesland)",value:"fr"},{label:"Drenthe",value:"dr"},{label:"Overijssel",value:"ov"},{label:"Flevoland",value:"fl"},{label:"Gelderland",value:"ge"},{label:"Utrecht",value:"ut"},{label:"Noord-Holland",value:"nh"},{label:"Zuid-Holland",value:"zh"},{label:"Zeeland",value:"ze"},{label:"Noord-Brabant",value:"nb"},{label:"Limburg",value:"li"}],description:"A province is required",registerOptions:{required:"You must select a province"}}},a={args:{label:"Disabled province",name:"province3",options:[{label:"Groningen",value:"gr"},{label:"Fryslân (Friesland)",value:"fr"},{label:"Drenthe",value:"dr"},{label:"Overijssel",value:"ov"},{label:"Flevoland",value:"fl"},{label:"Gelderland",value:"ge"},{label:"Utrecht",value:"ut"},{label:"Noord-Holland",value:"nh"},{label:"Zuid-Holland",value:"zh"},{label:"Zeeland",value:"ze"},{label:"Noord-Brabant",value:"nb"},{label:"Limburg",value:"li"}],disabled:!0}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Province',
+    name: 'province',
+    options: [{
+      label: 'Groningen',
+      value: 'gr'
+    }, {
+      label: 'Fryslân (Friesland)',
+      value: 'fr'
+    }, {
+      label: 'Drenthe',
+      value: 'dr'
+    }, {
+      label: 'Overijssel',
+      value: 'ov'
+    }, {
+      label: 'Flevoland',
+      value: 'fl'
+    }, {
+      label: 'Gelderland',
+      value: 'ge'
+    }, {
+      label: 'Utrecht',
+      value: 'ut'
+    }, {
+      label: 'Noord-Holland',
+      value: 'nh'
+    }, {
+      label: 'Zuid-Holland',
+      value: 'zh'
+    }, {
+      label: 'Zeeland',
+      value: 'ze'
+    }, {
+      label: 'Noord-Brabant',
+      value: 'nb'
+    }, {
+      label: 'Limburg',
+      value: 'li'
+    }],
+    registerOptions: {
+      required: 'A province is required'
+    },
+    wrapperProps: {}
+  }
+}`,...e.parameters?.docs?.source}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Province',
+    name: 'province1',
+    options: ['Groningen', 'Fryslân (Friesland)', 'Drenthe', 'Overijssel', 'Flevoland', 'Gelderland', 'Utrecht', 'Noord-Holland', 'Zuid-Holland', 'Zeeland', 'Noord-Brabant', 'Limburg'],
+    registerOptions: {
+      required: 'A province is required'
+    },
+    wrapperProps: {}
+  }
+}`,...l.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Province',
+    name: 'province2',
+    options: [{
+      label: 'Groningen',
+      value: 'gr'
+    }, {
+      label: 'Fryslân (Friesland)',
+      value: 'fr'
+    }, {
+      label: 'Drenthe',
+      value: 'dr'
+    }, {
+      label: 'Overijssel',
+      value: 'ov'
+    }, {
+      label: 'Flevoland',
+      value: 'fl'
+    }, {
+      label: 'Gelderland',
+      value: 'ge'
+    }, {
+      label: 'Utrecht',
+      value: 'ut'
+    }, {
+      label: 'Noord-Holland',
+      value: 'nh'
+    }, {
+      label: 'Zuid-Holland',
+      value: 'zh'
+    }, {
+      label: 'Zeeland',
+      value: 'ze'
+    }, {
+      label: 'Noord-Brabant',
+      value: 'nb'
+    }, {
+      label: 'Limburg',
+      value: 'li'
+    }],
+    description: 'A province is required',
+    registerOptions: {
+      required: 'You must select a province'
+    }
+  }
+}`,...n.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Disabled province',
+    name: 'province3',
+    options: [{
+      label: 'Groningen',
+      value: 'gr'
+    }, {
+      label: 'Fryslân (Friesland)',
+      value: 'fr'
+    }, {
+      label: 'Drenthe',
+      value: 'dr'
+    }, {
+      label: 'Overijssel',
+      value: 'ov'
+    }, {
+      label: 'Flevoland',
+      value: 'fl'
+    }, {
+      label: 'Gelderland',
+      value: 'ge'
+    }, {
+      label: 'Utrecht',
+      value: 'ut'
+    }, {
+      label: 'Noord-Holland',
+      value: 'nh'
+    }, {
+      label: 'Zuid-Holland',
+      value: 'zh'
+    }, {
+      label: 'Zeeland',
+      value: 'ze'
+    }, {
+      label: 'Noord-Brabant',
+      value: 'nb'
+    }, {
+      label: 'Limburg',
+      value: 'li'
+    }],
+    disabled: true
+  }
+}`,...a.parameters?.docs?.source}}};const N=["Primary","WithPlainArray","WithDescription","Disabled"];export{a as Disabled,e as Primary,n as WithDescription,l as WithPlainArray,N as __namedExportsOrder,y as default};
---
+++ demo-npm-updates/assets/TimeControl.stories-b2d9uU3_.js
@@ -0,0 +1,25 @@
+import{j as s}from"./iframe-OO432EJY.js";import{u as c,F as u,d as b}from"./index.esm-DQSCKQ9i.js";import"./preload-helper-PPVm8Dsz.js";const i=({label:o="Start time",name:a="example",description:n,registerOptions:m,wrapperProps:l,...d})=>{const p=c({defaultValues:{[a]:""}});return s.jsx(u,{...p,children:s.jsx(b,{label:o,name:a,description:n,registerOptions:m,wrapperProps:l,...d})})};i.__docgenInfo={description:"",methods:[],displayName:"TimeControl",props:{label:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"'Start time'",computed:!1}},name:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"'example'",computed:!1}}}};const q={title:"Components/TimeControl",component:i,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{label:{control:"text",description:"The checkbox label"},description:{control:"text",description:"Optional description text"},disabled:{control:"boolean"}}},e={args:{label:"Start time",name:"start_time",registerOptions:{required:"A start time is required"},wrapperProps:{}}},r={args:{label:"Start time",name:"start_time1",description:"Please provide a start time",registerOptions:{required:"A start time is required"}}},t={args:{label:"Disabled name",name:"start_time2",disabled:!0}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Start time',
+    name: 'start_time',
+    registerOptions: {
+      required: 'A start time is required'
+    },
+    wrapperProps: {}
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Start time',
+    name: 'start_time1',
+    description: 'Please provide a start time',
+    registerOptions: {
+      required: 'A start time is required'
+    }
+  }
+}`,...r.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Disabled name',
+    name: 'start_time2',
+    disabled: true
+  }
+}`,...t.parameters?.docs?.source}}};const _=["Primary","WithDescription","Disabled"];export{t as Disabled,e as Primary,r as WithDescription,_ as __namedExportsOrder,q as default};
