31cc76e3bb2a842d5f813ec224392a42cd2b797e
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2025-12-10 17:03:06 +0100

    Add SelectComparison component and Storybook stories for select controls

---
+++ storybook/src/Examples/AdsSelectVsReactSelect/SelectComparison.tsx
@@ -0,0 +1,77 @@
+import {
+  FormProvider,
+  ReactSelectControl,
+  SelectControl,
+} from '@amsterdam/ee-ads-rhf-lib';
+import { Column, Row } from '@amsterdam/design-system-react';
+import { useForm } from 'react-hook-form';
+import options from '../../utils/provinces';
+
+const SelectComparison = () => {
+  const form = useForm({
+    defaultValues: {
+      'ads-select-1': 'DR',
+      'react-select-1': 'DR',
+      'ads-select-2': 'NH',
+      'react-select-2': { label: 'Noord-Holland', value: 'NH' },
+      'ads-select-3': 'UT',
+      'react-select-3': [
+        { label: 'Utrecht', value: 'UT' },
+        { label: 'Zeeland', value: 'ZE' },
+      ],
+    },
+  });
+
+  return (
+    <FormProvider form={form} onSubmit={() => {}}>
+      <Column gap="large">
+        <Row>
+          <SelectControl
+            label="Provincies"
+            name="ads-select-1"
+            options={options || []}
+            registerOptions={{ required: 'Verplicht!' }}
+          />
+          <ReactSelectControl
+            label="Provincies"
+            name="react-select-1"
+            options={options || []}
+            registerOptions={{ required: 'Verplicht!' }}
+          />
+        </Row>
+        <Row>
+          <SelectControl
+            label="Provincies"
+            name="ads-select-2"
+            options={options || []}
+            registerOptions={{ required: 'Verplicht!' }}
+          />
+          <ReactSelectControl
+            label="Provincies"
+            name="react-select-2"
+            options={options || []}
+            registerOptions={{ required: 'Verplicht!' }}
+            isMulti={false}
+          />
+        </Row>
+        <Row>
+          <SelectControl
+            label="Provincies"
+            name="ads-select-3"
+            options={options || []}
+            registerOptions={{ required: 'Verplicht!' }}
+          />
+          <ReactSelectControl
+            label="Provincies"
+            name="react-select-3"
+            options={options || []}
+            registerOptions={{ required: 'Verplicht!' }}
+            isMulti={true}
+          />
+        </Row>
+      </Column>
+    </FormProvider>
+  );
+};
+
+export default SelectComparison;
