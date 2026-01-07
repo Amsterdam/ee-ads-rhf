fa9daf496cdc6b4e4f0ee7b679f0ebafb6aae2ca
Thomas Mills <mills.thoma@gmail.com>
2026-01-07 17:52:30 +0100

    useGrid and gridProps props added to FormProvider

--- packages/ee-ads-rhf/src/components/FormProvider/FormProvider.tsx
+++ packages/ee-ads-rhf/src/components/FormProvider/FormProvider.tsx
@@ -1,10 +1,10 @@
+import { Grid, GridProps } from '@amsterdam/design-system-react';
 import { PropsWithChildren } from 'react';
 import {
   FieldValues,
   FormProvider as RHFFormProvider,
   SubmitHandler,
   useForm,
-  // UseFormProps,
 } from 'react-hook-form';
 
 export interface FormProviderProps<TFieldValues extends FieldValues>
@@ -12,14 +12,29 @@ export interface FormProviderProps<TFieldValues extends FieldValues>
   form: ReturnType<typeof useForm<TFieldValues>>;
   onSubmit: SubmitHandler<TFieldValues>;
   noValidate?: boolean;
+  useGrid?: boolean;
+  gridProps?: GridProps;
 }
 
 export const FormProvider = <TFieldValues extends FieldValues>({
   form,
   onSubmit,
   noValidate = true,
+  useGrid = false,
+  gridProps = {},
   children,
 }: FormProviderProps<TFieldValues>) => {
+  if (useGrid) {
+    return (
+      <RHFFormProvider {...form}>
+        {/* Enable noValidate to prevent browser validation blocking JS */}
+        <form noValidate={noValidate} onSubmit={form.handleSubmit(onSubmit)}>
+          <Grid {...gridProps}>{children}</Grid>
+        </form>
+      </RHFFormProvider>
+    );
+  }
+
   return (
     <RHFFormProvider {...form}>
       {/* Enable noValidate to prevent browser validation blocking JS */}
