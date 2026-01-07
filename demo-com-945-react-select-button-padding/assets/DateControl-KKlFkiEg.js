763749f6c2477b19c2ace047b649d54507d20653
thomasm0 <mills.thoma@gmail.com>
2026-01-07 16:07:20 +0000

    Deploying to gh-pages from @ Amsterdam/ee-ads-rhf@b4b5842cf7de34cf331406c03cdaa3f130da8020 🚀

---
+++ demo-com-945-react-select-button-padding/assets/DateControl-KKlFkiEg.js
@@ -0,0 +1,13 @@
+import{j as n}from"./iframe-CZlpeA_0.js";import{useMDXComponents as r}from"./index-Cb0LOMla.js";import{M as i,P as a,C as d,a as t}from"./blocks-CA2TUZcn.js";import{D as l,W as m,a as c}from"./DateControl.stories-CembALJe.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C09pH-pe.js";import"./index-D8g69tAV.js";import"./TimeControl-CoTkYMie.js";function o(s){const e={a:"a",h1:"h1",h2:"h2",h3:"h3",li:"li",ul:"ul",...r(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:l}),`
+`,n.jsx(e.h1,{id:"datecontrol",children:"DateControl"}),`
+`,n.jsx(e.h2,{id:"references",children:"References"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"https://designsystem.amsterdam/?path=/docs/components-forms-date-input--docs",rel:"nofollow",children:"Amsterdam Design System - Date Input Docs"})}),`
+`]}),`
+`,n.jsx(a,{}),`
+`,n.jsx(d,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"with-description",children:"With Description"}),`
+`,n.jsx(t,{of:m}),`
+`,n.jsx(e.h3,{id:"disabled",children:"Disabled"}),`
+`,n.jsx(t,{of:c})]})}function M(s={}){const{wrapper:e}={...r(),...s.components};return e?n.jsx(e,{...s,children:n.jsx(o,{...s})}):o(s)}export{M as default};
