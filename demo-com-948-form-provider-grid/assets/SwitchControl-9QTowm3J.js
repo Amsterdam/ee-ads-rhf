cb9782d71a5f5370a1c18a33fd792d9ed5a06ba8
thomasm0 <mills.thoma@gmail.com>
2026-01-13 12:44:12 +0000

    Deploying to gh-pages from @ Amsterdam/ee-ads-rhf@1de8a44f64f2d472d8c1830ea4992126a4e858fa 🚀

---
+++ demo-com-948-form-provider-grid/assets/SwitchControl-9QTowm3J.js
@@ -0,0 +1,13 @@
+import{j as s}from"./iframe-donXh11V.js";import{useMDXComponents as o}from"./index-D4Ye-LNv.js";import{M as r,P as c,C as h,a as t}from"./blocks-ZbP9T3oM.js";import{S as a,W as l,D as m}from"./SwitchControl.stories-VwUEijtW.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C7zY1AHV.js";import"./index-CUWxe9FJ.js";import"./TimeControl-BB_ZdsD0.js";function i(e){const n={a:"a",h1:"h1",h2:"h2",h3:"h3",li:"li",ul:"ul",...o(),...e.components};return s.jsxs(s.Fragment,{children:[s.jsx(r,{of:a}),`
+`,s.jsx(n.h1,{id:"switchcontrol",children:"SwitchControl"}),`
+`,s.jsx(n.h2,{id:"references",children:"References"}),`
+`,s.jsxs(n.ul,{children:[`
+`,s.jsx(n.li,{children:s.jsx(n.a,{href:"https://designsystem.amsterdam/?path=/docs/components-forms-switch--docs",rel:"nofollow",children:"Amsterdam Design System - Switch Docs"})}),`
+`]}),`
+`,s.jsx(c,{}),`
+`,s.jsx(h,{}),`
+`,s.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,s.jsx(n.h3,{id:"with-description",children:"With Description"}),`
+`,s.jsx(t,{of:l}),`
+`,s.jsx(n.h3,{id:"disabled",children:"Disabled"}),`
+`,s.jsx(t,{of:m})]})}function C(e={}){const{wrapper:n}={...o(),...e.components};return n?s.jsx(n,{...e,children:s.jsx(i,{...e})}):i(e)}export{C as default};
