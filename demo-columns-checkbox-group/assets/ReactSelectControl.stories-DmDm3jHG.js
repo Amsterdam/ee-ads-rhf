e5171ac7975486cf26f71c5166ee087300c8fac5
remyvdwereld <86827854+remyvdwereld@users.noreply.github.com>
2026-02-26 14:24:19 +0000

    Deploying to gh-pages from @ Amsterdam/ee-ads-rhf@e5519682e0782cfd16a8c8dc112eab12efc1cca4 🚀

---
+++ demo-columns-checkbox-group/assets/ReactSelectControl.stories-DmDm3jHG.js
@@ -0,0 +1,40 @@
+import{j as n}from"./iframe-BqLz2nRS.js";import{u as q,b as y,R as x}from"./TimeControl-8qu-nyzJ.js";import{p as s}from"./provinces-BZRU43w1.js";const a=({label:l="Type",name:i="example",options:c=[],isMulti:p=!1,description:d,registerOptions:u,wrapperProps:m,hideFieldError:f=!1,hideErrorMessage:v=!1,shouldShow:b=!0,...g})=>{const h=q({defaultValues:{[i]:""}});return n.jsx(y,{...h,children:n.jsx(x,{label:l,name:i,options:c,isMulti:p,description:d,registerOptions:u,wrapperProps:m,hideFieldError:f,hideErrorMessage:v,shouldShow:b,...g})})};a.__docgenInfo={description:"",methods:[],displayName:"ReactSelectControl",props:{label:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"'Type'",computed:!1}},name:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"'example'",computed:!1}},options:{defaultValue:{value:"[]",computed:!1},required:!1},isMulti:{defaultValue:{value:"false",computed:!1},required:!1},hideFieldError:{defaultValue:{value:"false",computed:!1},required:!1},hideErrorMessage:{defaultValue:{value:"false",computed:!1},required:!1},shouldShow:{defaultValue:{value:"true",computed:!1},required:!1}}};const P={title:"Components/ReactSelectControl",component:a,tags:["autodocs"],argTypes:{name:{control:"text",description:"Form field name, used by React Hook Form for registration."},label:{control:"text",description:"Field label displayed above the select."},description:{control:"text",description:"Optional helper text displayed below the label."},options:{control:"object",description:"List of selectable options. Each option is an object `{ label, value }`"},isMulti:{control:"boolean",description:"Enable multiple selected options"},required:{control:"boolean",description:"Marks the field as required."},disabled:{control:"boolean",description:"Disables user interaction with the select."},registerOptions:{control:"object",description:"Validation rules passed directly to React Hook Form."},wrapperProps:{control:"object",description:"Generic props, for example, `className`, to apply to the `Field` wrapper component"},hideFieldError:{control:"boolean",default:"false",description:"Hides the invalid prop on the `Field` wrapper component"},hideErrorMessage:{control:"boolean",default:"false",description:"Hides the error message text"},shouldShow:{control:"boolean",default:"true",description:"Whether to render the field, useful for conditional form elements"},inputProps:{control:"object",description:"Optional additional props for the underlying `SelectInput` component."}}},e={args:{label:"Province",name:"province",options:s,registerOptions:{required:"A province is required"},isMulti:!0,wrapperProps:{}}},r={args:{label:"Province",name:"province2",options:s,description:"A province is required",registerOptions:{required:"You must select a province"}}},o={args:{label:"Province",name:"province3",options:s,description:"You can select multiple provinces",isMulti:!0,registerOptions:{required:"You must select a province"}}},t={args:{label:"Disabled province",name:"province4",options:s,disabled:!0}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Province',
+    name: 'province',
+    options: provinces,
+    registerOptions: {
+      required: 'A province is required'
+    },
+    isMulti: true,
+    wrapperProps: {}
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Province',
+    name: 'province2',
+    options: provinces,
+    description: 'A province is required',
+    registerOptions: {
+      required: 'You must select a province'
+    }
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Province',
+    name: 'province3',
+    options: provinces,
+    description: 'You can select multiple provinces',
+    isMulti: true,
+    registerOptions: {
+      required: 'You must select a province'
+    }
+  }
+}`,...o.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Disabled province',
+    name: 'province4',
+    options: provinces,
+    disabled: true
+  }
+}`,...t.parameters?.docs?.source}}};const S=["Primary","WithDescription","WithMultiple","Disabled"],F=Object.freeze(Object.defineProperty({__proto__:null,Disabled:t,Primary:e,WithDescription:r,WithMultiple:o,__namedExportsOrder:S,default:P},Symbol.toStringTag,{value:"Module"}));export{t as D,F as R,r as W,o as a};
