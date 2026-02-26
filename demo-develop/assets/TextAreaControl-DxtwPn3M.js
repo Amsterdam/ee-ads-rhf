55249549662c5cc752eb7e00dffe2440d54d72e0
thomasm0 <mills.thoma@gmail.com>
2026-02-26 16:50:25 +0000

    Deploying to gh-pages from @ Amsterdam/ee-ads-rhf@ba959a78757ba1eee65aab296dbd53353f628a37 🚀

---
+++ demo-develop/assets/TextAreaControl-DxtwPn3M.js
@@ -0,0 +1,13 @@
+import{j as e}from"./iframe-Ml0g321L.js";import{useMDXComponents as o}from"./index-DGNGt9Yb.js";import{M as i,P as a,C as l,a as t}from"./blocks-BCT8m915.js";import{T as m,W as c,D as d}from"./TextAreaControl.stories-BzcEvgXo.js";import"./preload-helper-PPVm8Dsz.js";import"./index-D0_vgE-g.js";import"./index-DARYJa00.js";import"./TimeControl-CgC8xIRc.js";function r(s){const n={a:"a",h1:"h1",h2:"h2",h3:"h3",li:"li",ul:"ul",...o(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:m}),`
+`,e.jsx(n.h1,{id:"textareacontrol",children:"TextAreaControl"}),`
+`,e.jsx(n.h2,{id:"references",children:"References"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://designsystem.amsterdam/?path=/docs/components-forms-text-area--docs",rel:"nofollow",children:"Amsterdam Design System - Text Area Docs"})}),`
+`]}),`
+`,e.jsx(a,{}),`
+`,e.jsx(l,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"with-description",children:"With Description"}),`
+`,e.jsx(t,{of:c}),`
+`,e.jsx(n.h3,{id:"disabled",children:"Disabled"}),`
+`,e.jsx(t,{of:d})]})}function M(s={}){const{wrapper:n}={...o(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(r,{...s})}):r(s)}export{M as default};
