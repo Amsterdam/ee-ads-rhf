73acd6bb3954a91620a40b8007ef40385d1684a3
thomasm0 <mills.thoma@gmail.com>
2026-03-23 16:13:20 +0000

    Deploying to gh-pages from @ Amsterdam/ee-ads-rhf@24d39ef7249a40fa5c301e3e56b2592b2a090adc 🚀

---
+++ demo-remove-form-disabled/assets/BookingForm.stories-sdekBSQY.js
@@ -0,0 +1,95 @@
+import{j as e,G as a,H as h,P as S,O as v,b as $,S as T,c as N,I as _,B,r as f,F as U,R as V,E as G,f as i,i as K,k as W}from"./iframe-Cd5HKE2U.js";import{i as C,g as F,D as I,h as P,T as J,u as Q,b as X}from"./TimeControl-sZXO1xqX.js";import{m as q,o as Y,s as g,a as Z}from"./schemas-KFaRowyI.js";import"./preload-helper-PPVm8Dsz.js";import"./index-9IIZBvqT.js";import"./index-vIxU6hOK.js";const ee="_root_1kms4_1",te="_loader_1kms4_12",E={root:ee,loader:te},L=()=>e.jsx("div",{className:E.root,"data-testid":"loader",children:e.jsx("div",{className:E.loader})});L.__docgenInfo={description:"",methods:[],displayName:"Loader"};const A=({onButtonClick:t})=>e.jsx(a,{as:"main",id:"inhoud",paddingBottom:"2x-large",paddingTop:"large",children:e.jsxs(a.Cell,{span:{narrow:4,medium:5,wide:7},start:{narrow:1,medium:2,wide:3},children:[e.jsx(h,{className:"ams-mb-m",level:1,children:"Waar u dit formulier voor gebruikt"}),e.jsx(S,{className:"ams-mb-xl",size:"large",children:"Met dit formulier maakt u een afspraak."}),e.jsx(h,{className:"ams-mb-s",level:2,children:"De stappen in dit formulier"}),e.jsxs(v,{className:"ams-mb-l",children:[e.jsxs(v.Item,{children:[e.jsx("strong",{children:"Uw gegevens"})," - Vul uw contactgegevens in."]}),e.jsxs(v.Item,{children:[e.jsx("strong",{children:"Afspraak"})," - Kies wanneer u een afspraak wilt maken."]}),e.jsxs(v.Item,{children:[e.jsx("strong",{children:"Controleren"})," - Controleer de gegevens die u heeft ingevuld. Verstuur de aanvraag."]})]}),e.jsx($,{href:"#",onClick:n=>{n.preventDefault(),t()},children:"Start het formulier"})]})});A.__docgenInfo={description:"",methods:[],displayName:"StepIntro",props:{onButtonClick:{required:!0,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""}}};const O=()=>e.jsx(a,{as:"main",id:"inhoud",paddingBottom:"2x-large",paddingTop:"large",children:e.jsx(a.Cell,{span:{narrow:4,medium:5,wide:7},start:{narrow:1,medium:2,wide:3},children:e.jsxs("header",{"aria-labelledby":"form-header",className:"ams-mb-m ams-gap-xs",children:[e.jsx(h,{"aria-hidden":!0,id:"form-header",level:2,size:"level-4",children:"Afspraak maken"}),e.jsx(S,{children:"Dank u voor uw inzending."})]})})});O.__docgenInfo={description:"",methods:[],displayName:"SuccessContent"};const z=({onPrevButtonClick:t,onNextButtonClick:n})=>{const{clearErrors:o,formState:{errors:m},trigger:r}=C(),d=Object.keys(m).length>0,u=q(m),c=async()=>{await r(["name","email"])&&n()},p=s=>{s.preventDefault(),o(),t()};return e.jsxs(e.Fragment,{children:[e.jsx(a,{className:"ams-mb-xl",children:e.jsx(a.Cell,{span:{narrow:4,medium:6,wide:7},start:{narrow:1,medium:2,wide:3},children:e.jsx(T,{href:"#",icon:N,onClick:p,children:"Vorige vraag"})})}),e.jsx(a,{as:"main",paddingBottom:"2x-large",children:e.jsxs(a.Cell,{span:{narrow:4,medium:6,wide:7},start:{narrow:1,medium:2,wide:3},children:[d&&e.jsx(_,{errors:u,headingLevel:4,className:"ams-mb-m","data-testid":"error-alert"}),e.jsxs("header",{"aria-labelledby":"form-header",className:"ams-mb-m ams-gap-xs",children:[e.jsx(h,{"aria-hidden":!0,id:"form-header",level:2,size:"level-4",children:"Afspraak maken"}),e.jsx(S,{children:"Stap 1 van 3: Uw gegevens"})]}),e.jsxs("form",{noValidate:!0,children:[e.jsx(F,{label:"Voornaam",name:"name",registerOptions:{required:!0},className:"ams-mb-m"}),e.jsx(F,{label:"E-mailadres",type:"text",inputMode:"email",autoComplete:"email",name:"email",registerOptions:{required:!0},className:"ams-mb-xl"}),e.jsx(B,{type:"button",onClick:c,children:"Volgende vraag"})]})]})})]})};z.__docgenInfo={description:"",methods:[],displayName:"StepPersonalDetails",props:{onPrevButtonClick:{required:!0,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""},onNextButtonClick:{required:!0,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""}}};const D={startDate:"Startdatum",startTime:"Starttijd",endDate:"Einddatum",endTime:"Eindtijd"},y=({children:t,legend:n,fields:o,className:m="ams-mb-m"})=>{const{formState:r,getValues:d}=C(),u=o.some(s=>r.errors[s]&&r.errors[s]?.message),c=o.filter(s=>r.errors[s]).map(s=>`${s}-error`).join(" "),p=f.useMemo(()=>{const s=[],x=[];for(const l of o){const k=r.errors?.[l]?.type;k&&(k==="required"||k==="too_small"&&d(l)===""?s.push(l):x.push(l))}const j=l=>l.length===0?"":l.length===1?`Het veld ${D[l[0]]} is verplicht.`:`De velden ${D[l[0]]} en ${D[l[1]]} zijn verplicht.`;return x.length?"De einddatum en -tijd moeten later zijn dan de startdatum en -tijd":j(s)||void 0},[r]);return e.jsxs(U,{legend:n,className:m,invalid:!!u,children:[u&&e.jsx(V,{children:e.jsx(G,{id:c,children:p})}),e.jsx(V,{children:t})]})};y.__docgenInfo={description:"",methods:[],displayName:"DateTimeFieldset",props:{legend:{required:!0,tsType:{name:"string"},description:""},fields:{required:!0,tsType:{name:"Array",elements:[{name:"string"}],raw:"string[]"},description:""},className:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"'ams-mb-m'",computed:!1}}}};const H=({minDateValue:t,onPrevButtonClick:n,onNextButtonClick:o})=>{const{clearErrors:m,formState:{errors:r},trigger:d,watch:u}=C(),c=Object.keys(r).length>0,p=q(r),[s]=u(["startDate"]),x=async()=>{await d(["startDate","startTime","endDate","endTime"])&&o()},j=w=>{w.preventDefault(),m(),n()};return e.jsxs(e.Fragment,{children:[e.jsx(a,{className:"ams-mb-xl",children:e.jsx(a.Cell,{span:{narrow:4,medium:6,wide:7},start:{narrow:1,medium:2,wide:3},children:e.jsx(T,{href:"#",icon:N,onClick:j,children:"Vorige vraag"})})}),e.jsx(a,{as:"main",paddingBottom:"2x-large",children:e.jsxs(a.Cell,{span:{narrow:4,medium:6,wide:7},start:{narrow:1,medium:2,wide:3},children:[c&&e.jsx(_,{errors:p,headingLevel:4,className:"ams-mb-m"}),e.jsxs("header",{"aria-labelledby":"form-header",className:"ams-mb-m ams-gap-xs",children:[e.jsx(h,{"aria-hidden":!0,id:"form-header",level:2,size:"level-4",children:"Afspraak maken"}),e.jsx(S,{children:"Stap 2 van 3: Afspraak"})]}),e.jsxs("form",{noValidate:!0,children:[e.jsxs(y,{legend:"Startdatum en -tijd",fields:["startDate","startTime"],children:[e.jsx(I,{label:"Startdatum",name:"startDate",registerOptions:{required:!0},min:t}),e.jsx(P,{label:"Starttijd",name:"startTime",registerOptions:{required:!0},hideErrorMessage:!0})]}),e.jsxs(y,{className:"ams-mb-xl",legend:"Einddatum-tijd",fields:["endDate","endTime"],children:[e.jsx(I,{label:"Einddatum",name:"endDate",registerOptions:{required:!0},min:s,hideErrorMessage:!0,hideFieldError:!0}),e.jsx(P,{label:"Eindtijd",name:"endTime",registerOptions:{required:!0},hideErrorMessage:!0})]}),e.jsx(B,{type:"button",onClick:x,children:"Volgende vraag"})]})]})})]})};H.__docgenInfo={description:"",methods:[],displayName:"StepAppointment",props:{minDateValue:{required:!0,tsType:{name:"string"},description:""},onPrevButtonClick:{required:!0,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""},onNextButtonClick:{required:!0,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""}}};const M=({onPrevButtonClick:t,onSubmit:n})=>{const{getValues:o,handleSubmit:m}=C(),r=o();return e.jsxs(e.Fragment,{children:[e.jsx(a,{className:"ams-mb-xl",children:e.jsx(a.Cell,{span:{narrow:4,medium:6,wide:7},start:{narrow:1,medium:2,wide:3},children:e.jsx(T,{href:"#",icon:N,onClick:d=>{d.preventDefault(),t()},children:"Vorige vraag"})})}),e.jsx(a,{as:"main",paddingBottom:"2x-large",children:e.jsxs(a.Cell,{span:{narrow:4,medium:6,wide:7},start:{narrow:1,medium:2,wide:3},children:[e.jsxs("header",{"aria-labelledby":"form-header",className:"ams-mb-m ams-gap-xs",children:[e.jsx(h,{"aria-hidden":!0,id:"form-header",level:2,size:"level-4",children:"Afspraak maken"}),e.jsx(S,{children:"Stap 3 van 3: Controleren"})]}),e.jsxs(i,{className:"ams-mb-l",children:[e.jsx(i.Caption,{children:e.jsx(h,{level:4,children:"Controleer uw gegevens"})}),e.jsxs(i.Body,{children:[e.jsxs(i.Row,{children:[e.jsx(i.Cell,{children:"Voornaam"}),e.jsx(i.Cell,{children:r.name})]}),e.jsxs(i.Row,{children:[e.jsx(i.Cell,{children:"E-mailadres"}),e.jsx(i.Cell,{children:r.email})]}),e.jsxs(i.Row,{children:[e.jsx(i.Cell,{children:"Startdatum en -tijd"}),e.jsx(i.Cell,{children:new Date(`${r.startDate}T${r.startTime}`).toLocaleString("nl-NL",{day:"numeric",month:"long",year:"numeric",hour:"2-digit",minute:"2-digit",hour12:!1}).replace(",","")})]}),e.jsxs(i.Row,{children:[e.jsx(i.Cell,{children:"Einddatum en -tijd"}),e.jsx(i.Cell,{children:new Date(`${r.endDate}T${r.endTime}`).toLocaleString("nl-NL",{day:"numeric",month:"long",year:"numeric",hour:"2-digit",minute:"2-digit",hour12:!1}).replace(",","")})]})]})]}),e.jsxs("form",{noValidate:!0,onSubmit:m(n),children:[e.jsx(J,{label:"Opmerkingen",name:"comments",className:"ams-mb-xl"}),e.jsx(B,{type:"submit",children:"Verzenden"})]})]})})]})};M.__docgenInfo={description:"",methods:[],displayName:"StepConfirm",props:{onPrevButtonClick:{required:!0,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""},onSubmit:{required:!0,tsType:{name:"SubmitHandler",elements:[{name:"FieldValues"}],raw:"SubmitHandler<FieldValues>"},description:""}}};const re=Y({name:g().min(1,"Vul uw voornaam in"),email:g().email("Vul een geldig e-mailadres in, bijvoorbeeld naam@voorbeeld.nl"),startDate:g().min(1,"Vul een startdatum in"),startTime:g().min(1,"Vul een starttijd in"),endDate:g().min(1,"Vul een einddatum in"),endTime:g().min(1,"Vul een eindtijd in"),comments:g().optional()}).superRefine((t,n)=>{if(!t.startDate||!t.startTime||!t.endDate||!t.endTime)return;const o=new Date(`${t.startDate}T${t.startTime}`);if(new Date(`${t.endDate}T${t.endTime}`)<=o){const r=t.startDate===t.endDate;n.addIssue({code:"custom",message:"De einddatum en -tijd moeten later zijn dan de startdatum en -tijd",path:r?["endTime"]:["endDate"]})}}),R=()=>{const[t,n]=f.useState(0),[o,m]=f.useState(!1),r=f.useRef(!1),d=new Date,u=new Date().toISOString().split("T")[0],c=Q({resolver:Z(re),defaultValues:{name:"",email:"",startDate:d.toISOString().split("T")[0],startTime:"",endDate:"",endTime:"",comments:""}}),p=f.useCallback(async()=>{try{if(r.current)return;r.current=!0,setTimeout(()=>{m(!0),r.current=!1},1500)}catch(j){console.log("form error!",j)}},[c.formState.isSubmitting]),s=()=>{n(t+1)},x=[e.jsx(A,{onButtonClick:()=>n(1)},"step-0"),e.jsx(z,{onPrevButtonClick:()=>n(0),onNextButtonClick:s},"step-1"),e.jsx(H,{minDateValue:u,onPrevButtonClick:()=>n(1),onNextButtonClick:s},"step-2"),e.jsx(M,{onPrevButtonClick:()=>n(2),onSubmit:p},"step-3")];return e.jsxs(K,{children:[e.jsx(W,{className:"ams-mb-xl"}),c.formState.isSubmitting&&!o&&e.jsx(L,{}),e.jsx(X,{...c,children:o?e.jsx(O,{}):x[t]})]})};R.__docgenInfo={description:"",methods:[],displayName:"BookingForm"};const le={title:"Examples/BookingForm",component:R,parameters:{layout:"fullscreen",docs:{source:{code:`
+          import { useCallback, useState } from 'react';
+          import {
+            SubmitHandler,
+            useForm,
+            FormProvider,
+            FieldValues,
+          } from 'react-hook-form';
+          import { zodResolver } from '@hookform/resolvers/zod';
+          import { Page, PageHeader } from '@amsterdam/design-system-react';
+          import Loader from './components/Loader/Loader';
+          import StepIntro from './components/StepIntro/StepIntro';
+          import SuccessContent from './components/SuccessContent/SuccessContent';
+          import StepPersonalDetails from './components/StepPersonalDetails/StepPersonalDetails';
+          import StepAppointment from './components/StepAppointment/StepAppointment';
+          import StepConfirm from './components/StepConfirm/StepConfirm';
+          import bookingFormSchema, { BookingFormData } from './schema';
+
+          const BookingForm = () => {
+            const [currentStep, setCurrentStep] = useState(0);
+            const [isLoading, setIsLoading] = useState(false);
+            const [isSubmitted, setIsSubmitted] = useState(false);
+
+            const nowDateTime = new Date();
+            const nowDate = new Date().toISOString().split('T')[0];
+
+            const form = useForm<BookingFormData>({
+              resolver: zodResolver(bookingFormSchema),
+              defaultValues: {
+                name: '',
+                email: '',
+                startDate: nowDateTime.toISOString().split('T')[0],
+                startTime: '',
+                endDate: '',
+                endTime: '',
+                comments: '',
+              },
+            });
+
+            const handleSubmit: SubmitHandler<FieldValues> = useCallback(async () => {
+              try {
+                /**
+                 * Use setTimeout to Simulate API call
+                 * - Here's where validation can happen
+                 * - Here's where you can show a post-submission success component
+                 * or redirect the user to a new page
+                 */
+                setIsLoading(true);
+
+                setTimeout(() => {
+                  setIsLoading(false);
+                  setIsSubmitted(true);
+                }, 1500);
+              } catch (error) {
+                console.log('form error!', error);
+              }
+            }, []);
+
+            const handleNextStep = () => {
+              setCurrentStep(currentStep + 1);
+            };
+
+            const steps = [
+              <StepIntro onButtonClick={() => setCurrentStep(1)} key="step-0" />,
+              <StepPersonalDetails
+                onPrevButtonClick={() => setCurrentStep(0)}
+                onNextButtonClick={handleNextStep}
+                key="step-1"
+              />,
+              <StepAppointment
+                minDateValue={nowDate}
+                onPrevButtonClick={() => setCurrentStep(1)}
+                onNextButtonClick={handleNextStep}
+                key="step-2"
+              />,
+              <StepConfirm
+                onPrevButtonClick={() => setCurrentStep(2)}
+                onSubmit={handleSubmit}
+                key="step-3"
+              />,
+            ];
+
+            return (
+              <Page>
+                <PageHeader className="ams-mb-xl" />
+                {isLoading && !isSubmitted && <Loader />}
+                <FormProvider {...form}>
+                  {!isSubmitted ? steps[currentStep] : <SuccessContent />}
+                </FormProvider>
+              </Page>
+            );
+          };
+        `}}},tags:["autodocs"]},b={args:{}};b.parameters={...b.parameters,docs:{...b.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...b.parameters?.docs?.source}}};const de=["Primary"];export{b as Primary,de as __namedExportsOrder,le as default};
---
+++ demo-remove-form-disabled/assets/index-9IIZBvqT.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-Cd5HKE2U.js";import{r as o}from"./index-vIxU6hOK.js";var t=o();const m=r(t);export{m as F,t as r};
