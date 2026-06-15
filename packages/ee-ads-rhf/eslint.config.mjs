a33150c75f8625acbcab9f5976e846c1b2dd83cb
Thomas Mills <mills.thoma@gmail.com>
2026-06-15 14:18:25 +0200

    align storybook package versions to fix ci build pain train?

--- packages/ee-ads-rhf/eslint.config.mjs
+++ packages/ee-ads-rhf/eslint.config.mjs
@@ -1,13 +1,3 @@
 import baseConfig from '../../eslint.config.mjs';
-import storybook from 'eslint-plugin-storybook';
 
-export default [
-  ...baseConfig,
-  {
-    files: ['**/*.stories.@(js|jsx|ts|tsx|mjs|cjs)'],
-    ...storybook.configs['flat/recommended'][0],
-  },
-  {
-    ignores: ['!.storybook'],
-  },
-];
+export default [...baseConfig];
--- storybook/config/main.ts
+++ storybook/config/main.ts
@@ -5,7 +5,6 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    getAbsolutePath('@chromatic-com/storybook'),
     getAbsolutePath('@storybook/addon-docs'),
     getAbsolutePath('@storybook/addon-a11y'),
     getAbsolutePath('@storybook/addon-vitest'),
